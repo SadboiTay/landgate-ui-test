1dc1ee0a30b243d04f0355596e9fe4bf41347d57
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2023-07-18 10:57:27 -0600

    person model accepts dataIn constructor

--- src/app/models/person-model.ts
+++ src/app/models/person-model.ts
@@ -5,8 +5,13 @@ export class PersonModel {
   public age: number;
   public jobTitle: string;
 
-  constructor(dataIn: any) {
+  constructor(dataIn: PersonModel) {
     // TODO: Implement a dataIn object that gets passed in as a JavaScript Object
-
+    if (dataIn) {
+      this.firstName = dataIn.firstName;
+      this.lastName = dataIn.lastName;
+      this.age = dataIn.age;
+      this.jobTitle = dataIn.jobTitle;
+    }
   }
 }
