eceb6cad66b6394f3867fcdbfc6ed7727f884377
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2023-07-18 10:54:33 -0600

    flush out reducers/state

--- src/app/components/people-ngrx/ngrx/reducers/people.reducer.ts
+++ src/app/components/people-ngrx/ngrx/reducers/people.reducer.ts
@@ -1,22 +1,30 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import * as PeopleActions from '../actions/people.actions';
+import { PersonModel } from '../../../../models/person-model';
 
 export const peopleFeatureKey = 'people';
 
 // TODO: Need to add People to the store and initialize it.
 export interface State {
-
+  isLoading: boolean,
+  people: PersonModel[]
 }
 
 export const initialState: State = {
-
+  isLoading: false,
+  people: [],
 };
 
 
 export const reducer = createReducer(
   initialState,
 
-  on(PeopleActions.loadPeoples, state => state),
+  on(PeopleActions.loadPeoples, (state) => ({ ...state, isLoading: true })),
 
+  on(PeopleActions.loadedPeople, (state, { people }) => ({
+    ...state,
+    people: people,
+    isLoading: false,
+  }))
 );
 
